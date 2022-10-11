77e1c0e817e54c9594a701e233e1a740f2788491
mnashemhlanga <105486599+mnashemhlanga@users.noreply.github.com>
2022-10-11 05:41:31 +0200

    solutions to tasks

---
+++ 3-quick_sort.c
@@ -0,0 +1,89 @@
+#include<stddef.h>
+#include<stdlib.h>
+#include "sort.h"
+
+void swap(int *array, int a, int b, size_t size);
+int partition(int *array, int lb, int ub, size_t size);
+void q_sort(int *array, int lb, int ub, size_t size);
+
+/**
+ * quick_sort - sort an array of integers using quick sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	q_sort(array, 0, size - 1, size);
+}
+
+/**
+ * swap - swap array elements
+ * @array: the array whose elements are to be swapped
+ * @a: the first element
+ * @b: the second element
+ * @size: size of the array
+ */
+
+void swap(int *array, int a, int b, size_t size)
+{
+	if (array[a] != array[b])
+	{
+		int temp = array[a];
+
+		array[a] = array[b];
+		array[b] = temp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * partition - perform the lumuto partitioning
+ * @array: the array to partition
+ * @lb: the lower bound
+ * @ub: the upper bound
+ * @size: the size of the array
+ *
+ * Return: the position between two partitions
+ */
+
+int partition(int *array, int lb, int ub, size_t size)
+{
+	int pivot = array[ub];
+	int j = lb - 1;
+	int i;
+
+	for (i = lb; i < ub; i++)
+	{
+		if (array[i] < pivot)
+		{
+			j += 1;
+			swap(array, i, j, size);
+		}
+	}
+	swap(array, ub, j + 1, size);
+	return (j + 1);
+}
+
+/**
+ * q_sort - perform the sorting
+ * @array: the array to sort
+ * @lb: the lower bound
+ * @ub: the upper bound
+ * @size:; the size of the array
+ */
+
+void q_sort(int *array, int lb, int ub, size_t size)
+{
+	int part;
+
+	if (lb < ub)
+	{
+		part = partition(array, lb, ub, size);
+		q_sort(array, lb, part - 1, size);
+		q_sort(array, part + 1, ub, size);
+	}
+}
